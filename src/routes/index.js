c8392d3dc6f2b6d2ebb14774f3bc969a1f1ff268
Rashomon <mohammad18018@mail.unpad.ac.id>
2021-05-17 09:43:41 +0700

    error

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { getByProduct } = require('../controllers/LaptopController');
+
+
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        success: true,
+        data: 'Qrary API is Already Running...'
+    });
+});
+
+router.get('/test', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        success: true,
+        data: 'Qrary API is Already Running...'
+    });
+});
+
+// Endpoint mendapatkan data semua buku atau berdasarkan param atau query
+
+
+// router.post('/type', getByType);
+// router.post('/cpu', getByCpu);
+// router.post('/inches', getByInches);
+
+router.post('/product', getByProduct);
+// router.post('/company', getByCompany);
+module.exports = router;
